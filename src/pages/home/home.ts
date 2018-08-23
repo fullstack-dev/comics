3c69b4c55147460cae92440c839ef017d82d4c79
fullstack-dev <hall0711@hotmail.com>
2018-08-23 12:51:05 +0100

    - integrating pouchDB

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { DatabaseProvider } from '../../providers/database/database';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,45 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	public hasComics     : boolean = false;
+ 	public comics        : any;
+
+  constructor(
+  	public navCtrl: NavController,
+  	public DB     : DatabaseProvider) {
 
   }
+
+  ionViewWillEnter()
+ 	{
+    this.displayComics();
+ 	}
+
+ 	displayComics()
+ 	{
+    this.DB.retrieveComics().then((data)=>
+    {
+     	let existingData = Object.keys(data).length;
+     	if(existingData !== 0)
+   		{
+        this.hasComics  = true;
+        this.comics   = data;
+   		}
+	   	else
+	   	{
+      	console.log("we get nada!");
+   		}
+  	});
+ 	}
+
+ 	addCharacter()
+ 	{
+    this.navCtrl.push('AddPage');
+ 	}
+
+ 	viewCharacter(param)
+ 	{
+    this.navCtrl.push('AddPage', param);
+ 	}
 
 }
